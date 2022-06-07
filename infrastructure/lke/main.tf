5b1686c2f601a0168dbf821c3d5abe4811acde0b
Tino Breddin <t@tolbrino.com>
2022-06-07 11:29:22 +0200

    Add missing variable definition

--- infrastructure/lke/main.tf
+++ infrastructure/lke/main.tf
@@ -1,6 +1,6 @@
 module "lke" {
   source  = "app.terraform.io/hoprnet/lke/linode"
-  version = "0.1.1"
+  version = "0.1.2"
 
   cluster_name  = "playground"
   machine_type  = "g6-standard-8"
---
+++ infrastructure/lke/variables.tf
@@ -0,0 +1,4 @@
+variable "linode_api_token" {
+  type        = string
+  description = "api token used by the linode provider"
+}
