56b21b2ad955d8d6fba96399c76d2223831485ac
Tino Breddin <t@tolbrino.com>
2022-06-07 15:15:07 +0200

    infrastructure(main): add local test setup

--- infrastructure/main/providers.tf
+++ infrastructure/main/providers.tf
@@ -1,3 +1,20 @@
+locals {
+  kube_config                        = yamldecode(data.terraform_remote_state.playground-lke.outputs.k8s_config)
+  kube_config_token                  = local.kube_config.users.0.user.token
+  kube_config_host                   = local.kube_config.clusters.0.cluster.server
+  kube_config_cluster_ca_certificate = base64decode(local.kube_config.clusters.0.cluster.certificate-authority-data)
+}
+
+data "terraform_remote_state" "playground-lke" {
+  backend = "remote"
+  config = {
+    organization = "hoprnet"
+    workspaces = {
+      name = "playground-lke"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = local.kube_config_host
