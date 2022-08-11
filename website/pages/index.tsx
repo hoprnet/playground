bb703fe5e46de5f10ddfc23f6ec316ba65f34eec
Michal Jadach <michal.jadach@hoprnet.org>
2022-08-11 10:55:08 +0200

    playground-fe: code formatting

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,85 +1,88 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { isSSR, getUrlParams, secondsToTime } from "../src/utils";
 
 // Components
-import Layout from '../src/future-hopr-lib-components/Layout'
+import Layout from "../src/future-hopr-lib-components/Layout";
 import Introduction from "../src/pages/introduction";
 import Cluster from "../src/pages/cluster";
 
 //Data
-import placeholderResponse from '../src/data/placeholderActivationResponse.json';
-import { dapps } from '../src/data/dapps';
+import placeholderResponse from "../src/data/placeholderActivationResponse.json";
+import { dapps } from "../src/data/dapps";
 const { NEXT_PUBLIC_API_ENDPOINT } = process.env;
 import placeholderMacIcons from "../src/data/placeholderMacIcons.json";
 
 //Types
 import type { NextPage } from "next";
-import {Clusters, ClustersAvailability, Apps} from '../src/types';
-
+import { Clusters, ClustersAvailability, Apps } from "../src/types";
 
 const Index: NextPage = () => {
   const clusterOn = useRef(false);
   const [clusters, set_clusters] = useState<Clusters>([]);
   const [peerIds, set_peerIds] = useState<string[]>([]);
   const [clustersValidUntil, set_clustersValidUntil] = useState<number>(0);
-  const [clustersAvailability, set_clustersAvailability] = useState<ClustersAvailability>({
+  const [clustersAvailability, set_clustersAvailability] =
+    useState<ClustersAvailability>({
       total: 0,
       used: 0,
       available: 0,
-   //  available: 1, //dev
+      //  available: 1, //dev
       secondsUntilRelease: 0,
-  });
+    });
   const urlParams = !isSSR ? getUrlParams(location) : {};
   const api_url = urlParams.api || NEXT_PUBLIC_API_ENDPOINT || "";
 
-    useEffect(() => {
-        // Pre-release redirect
-        if(process.env.NEXT_PUBLIC_REDIRECT === '1') { window.location.replace("https://hoprnet.org/")}
+  useEffect(() => {
+    // Pre-release redirect
+    if (process.env.NEXT_PUBLIC_REDIRECT === "1") {
+      window.location.replace("https://hoprnet.org/");
+    }
 
-        //Get clusters from localStorage
-        if(localStorage["clustersValidUntil"]){
-            const leftSeconds = parseInt(localStorage["clustersValidUntil"]) - Date.now()/1000;
-            if(leftSeconds > 60) {
-                clusterOn.current = true;
-                set_clustersValidUntil(parseInt(localStorage["clustersValidUntil"]));
-                set_clusters(JSON.parse(localStorage["clusters"]));
-            }
-        }
+    //Get clusters from localStorage
+    if (localStorage["clustersValidUntil"]) {
+      const leftSeconds =
+        parseInt(localStorage["clustersValidUntil"]) - Date.now() / 1000;
+      if (leftSeconds > 60) {
+        clusterOn.current = true;
+        set_clustersValidUntil(parseInt(localStorage["clustersValidUntil"]));
+        set_clusters(JSON.parse(localStorage["clusters"]));
+      }
+    }
 
-        //Get clusters
+    //Get clusters
+    getClusterAvailability();
+    const interval = setInterval(() => {
+      if (!clusterOn.current) {
         getClusterAvailability();
-        const interval = setInterval(() => {
-            if(!clusterOn.current) {
-                getClusterAvailability();
-            }
-        }, 5000);
-        return () => clearInterval(interval);
-    }, []);
+      }
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
-    useEffect(( ) => {
-        if(clusters.length > 0){
-            getPeerIds(clusters);
-        }
-    }, [clusters]);
+  useEffect(() => {
+    if (clusters.length > 0) {
+      getPeerIds(clusters);
+    }
+  }, [clusters]);
 
-    const getClusterAvailability = () => {
-      const api_url_clusters = `${api_url}/api/clusters`;
-      const req = new Request(api_url_clusters);
-      fetch(req)
-          .then(response => response.json())
-          .then(data => {
-              //console.log(`/clusters Resp:`, data);
-              set_clustersAvailability({
-                  total: data.total_clusters_count,
-                  used: data.total_clusters_count - data.free_clusters_count,
-                  available: data.free_clusters_count,
-                  secondsUntilRelease: data.seconds_until_release,
-              });
-          })
-          .catch(err => {
-              console.error(`API call failed with ${err}`)
-          });
-  }
+  const getClusterAvailability = () => {
+    const api_url_clusters = `${api_url}/api/clusters`;
+    const req = new Request(api_url_clusters);
+    fetch(req)
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(`/clusters Resp:`, data);
+        set_clustersAvailability({
+          total: data.total_clusters_count,
+          used: data.total_clusters_count - data.free_clusters_count,
+          available: data.free_clusters_count,
+          secondsUntilRelease: data.seconds_until_release,
+        });
+      })
+      .catch((err) => {
+        console.error(`API call failed with ${err}`);
+      });
+  };
 
   const launchCluster = () => {
     console.log(`launchCluster`);
@@ -87,110 +90,112 @@ const Index: NextPage = () => {
     console.log(`Calling API at ${api_url_clusters}`);
 
     const req = new Request(api_url_clusters);
-    fetch(req, { method: 'POST'})
-        .then(response => response.json())
-        .then(data => {
-          console.log(`/activate Resp:`, data);
-          clusterOn.current = true;
-          set_clusters(data.cluster_nodes);
-          set_clustersValidUntil(data.cluster_valid_until);
-          localStorage.setItem('clusters', JSON.stringify(data.cluster_nodes));
-          localStorage.setItem('clustersValidUntil', data.cluster_valid_until);
-        })
-        .catch(err => {
-          console.error(`API call failed with ${err}`)
-        });
+    fetch(req, { method: "POST" })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`/activate Resp:`, data);
+        clusterOn.current = true;
+        set_clusters(data.cluster_nodes);
+        set_clustersValidUntil(data.cluster_valid_until);
+        localStorage.setItem("clusters", JSON.stringify(data.cluster_nodes));
+        localStorage.setItem("clustersValidUntil", data.cluster_valid_until);
+      })
+      .catch((err) => {
+        console.error(`API call failed with ${err}`);
+      });
 
     // DEV only
     // sclusterOn.current = true;
     // set_clusters(placeholderResponse.cluster_nodes);
-
   };
 
-    const getPeerIds = async (clusters: Clusters) => {
-        setTimeout(async ()=>{
-            let peerIdsTmp:string[] = [];
-            for (let i = 0; i < clusters.length; i++) {
-                //  for (let i = 0; i < 2; i++){
-                console.log('clusters', clusters);
-                //       const req = new Request();
-                await fetch(`${clusters[i].api_url}/api/v2/account/addresses`, {
-                    "method": 'GET',
-                    "headers": {
-                        "accept": "*/*",
-                        "authorization": `Basic ${btoa(clusters[i].api_token)}`,
-                    },
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        peerIdsTmp.push(data.hoprAddress);
-                    })
-                    .catch(err => {
-                        console.error(`API call failed with ${err}`)
-                    });
-            }
+  const getPeerIds = async (clusters: Clusters) => {
+    setTimeout(async () => {
+      let peerIdsTmp: string[] = [];
+      for (let i = 0; i < clusters.length; i++) {
+        //  for (let i = 0; i < 2; i++){
+        console.log("clusters", clusters);
+        //       const req = new Request();
+        await fetch(`${clusters[i].api_url}/api/v2/account/addresses`, {
+          method: "GET",
+          headers: {
+            accept: "*/*",
+            authorization: `Basic ${btoa(clusters[i].api_token)}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            peerIdsTmp.push(data.hoprAddress);
+          })
+          .catch((err) => {
+            console.error(`API call failed with ${err}`);
+          });
+      }
 
-            if(peerIdsTmp.length !== clusters.length) {
-                await getPeerIds(clusters);
-            } else {
-                console.log('peerIds', peerIdsTmp);
-                set_peerIds(peerIdsTmp);
-            }
-        }, 1000)
-    };
+      if (peerIdsTmp.length !== clusters.length) {
+        await getPeerIds(clusters);
+      } else {
+        console.log("peerIds", peerIdsTmp);
+        set_peerIds(peerIdsTmp);
+      }
+    }, 1000);
+  };
 
   const parseApps = (apps: Apps, clusters: Clusters) => {
-  //  console.log('parseApps', apps, clusters);
-
     let nodeUrlParams = [];
     for (let i = 0; i < clusters.length; i++) {
-      nodeUrlParams.push(`?apiEndpoint=${encodeURIComponent(clusters[i].api_url)}&apiToken=${encodeURIComponent(clusters[i].api_token)}`)
+      nodeUrlParams.push(
+        `?apiEndpoint=${encodeURIComponent(
+          clusters[i].api_url
+        )}&apiToken=${encodeURIComponent(clusters[i].api_token)}`
+      );
     }
 
     let parsedApps = [];
     for (let i = 0; i < apps.length; i++) {
-        let icon: string | undefined = placeholderMacIcons[i].icon;
-        if (apps[i].icon) icon = apps[i].icon;
-
-      if(apps[i].name === 'hoprd-admin') {
-          parsedApps.push({
-            key: apps[i].key,
-            name: apps[i].name,
-            text: apps[i].text,
-            icon,
-            links: clusters.map(cluster => cluster.admin_url + `?apiEndpoint=${encodeURIComponent(cluster.api_url)}&apiToken=${encodeURIComponent(cluster.api_token)}`)
-        })
+      let icon: string | undefined = placeholderMacIcons[i].icon;
+      if (apps[i].icon) icon = apps[i].icon;
+      if (apps[i].name === "hoprd-admin") {
+        parsedApps.push({
+          key: apps[i].key,
+          name: apps[i].name,
+          text: apps[i].text,
+          icon,
+          links: clusters.map(
+            (cluster) =>
+              cluster.admin_url +
+              `?apiEndpoint=${encodeURIComponent(
+                cluster.api_url
+              )}&apiToken=${encodeURIComponent(cluster.api_token)}`
+          ),
+        });
       } else {
-          parsedApps.push(
-              {
-                  key: apps[i].key,
-                  name: apps[i].name,
-                  text: apps[i].text,
-                  icon,
-                  links: nodeUrlParams.map(params => apps[i].url + params)
-              }
-          )
+        parsedApps.push({
+          key: apps[i].key,
+          name: apps[i].name,
+          text: apps[i].text,
+          icon,
+          links: nodeUrlParams.map((params) => apps[i].url + params),
+        });
       }
     }
-
-  //  console.log('parsedApps', parsedApps);
     return parsedApps;
-  }
+  };
 
   return (
     <Layout>
-      { !clusterOn.current ? (
+      {!clusterOn.current ? (
         <Introduction
           clusters={clustersAvailability}
           launchCluster={launchCluster}
         />
       ) : (
         <Cluster
-            clustersAvailability={clustersAvailability}
-            clusters={clusters}
-            apps={parseApps(dapps, clusters)}
-            clustersValidUntil={clustersValidUntil}
-            peerIds={peerIds}
+          clustersAvailability={clustersAvailability}
+          clusters={clusters}
+          apps={parseApps(dapps, clusters)}
+          clustersValidUntil={clustersValidUntil}
+          peerIds={peerIds}
         />
       )}
     </Layout>
--- website/src/pages/cluster.tsx
+++ website/src/pages/cluster.tsx
@@ -1,32 +1,32 @@
 import React, { useState, useMemo, useEffect } from "react";
-import Router from 'next/router'
+import Router from "next/router";
 import styles from "../../styles/pages/cluster.module.scss";
 import { secondsToTime } from "../utils";
 
 //Components
 import Playground from "../components/playground";
 import LinkHolder from "../components/link-holder";
-import Section from "../future-hopr-lib-components/Section"
-import Dock from "../future-hopr-lib-components/Dock"
-import CopyButton from '../future-hopr-lib-components/CopyButton'
+import Section from "../future-hopr-lib-components/Section";
+import Dock from "../future-hopr-lib-components/Dock";
+import CopyButton from "../future-hopr-lib-components/CopyButton";
 
 //Types
-import { App, Apps, Clusters, ClustersAvailability, Links } from '../types'
+import { App, Apps, Clusters, ClustersAvailability, Links } from "../types";
 
 const Cluster = (props: {
-  clustersAvailability: ClustersAvailability
-  apps: Apps,
-  clusters: Clusters,
-  clustersValidUntil: number,
-  peerIds: string[]
+  clustersAvailability: ClustersAvailability;
+  apps: Apps;
+  clusters: Clusters;
+  clustersValidUntil: number;
+  peerIds: string[];
 }) => {
   const [selection, set_selection] = useState<number>(-1);
-  const [timeRemaining, set_timeRemaining] = useState<string>('20:00');
+  const [timeRemaining, set_timeRemaining] = useState<string>("20:00");
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const leftSeconds = props.clustersValidUntil - Date.now()/1000;
-      if(leftSeconds < 0) {
+      const leftSeconds = props.clustersValidUntil - Date.now() / 1000;
+      if (leftSeconds < 0) {
         // @ts-ignore
         Router.reload(window.location.pathname);
       } else {
@@ -39,62 +39,88 @@ const Cluster = (props: {
   const links: Links = selection !== -1 ? props.apps[selection].links : [];
 
   return (
-    <Section
-        gradient
-    >
+    <Section gradient>
       {/* welcome section */}
       <div className={`container section ${styles.welcome}`}>
         <Playground />
         <div>
           <div className={styles.dAppDockTitle}>WELCOME TO YOUR PLAYGROUND</div>
-          Your cluster has started! You can now choose one of the dApps below and start exploring it.
-          <br/><br/>
+          Your cluster has started! You can now choose one of the dApps below
+          and start exploring it.
+          <br />
+          <br />
           Be aware that your session will be terminated automatically after 20
-          minutes. <br/><br/>Time remaining:{" "}
-          <span className="highlight">{timeRemaining}</span>.
+          minutes. <br />
+          <br />
+          Time remaining: <span className="highlight">{timeRemaining}</span>.
         </div>
       </div>
 
       {/* show apps */}
-      <div className={styles.dAppDockTitle}><strong>PLAYGROUND DAPP DOCK</strong></div>
-      <div className={styles.dAppDockSubtitle}>Choose which dApp you`d like to play first</div>
-      <Dock
-        apps={props.apps}
-        iconClicked={set_selection}
-      />
+      <div className={styles.dAppDockTitle}>
+        <strong>PLAYGROUND DAPP DOCK</strong>
+      </div>
+      <div className={styles.dAppDockSubtitle}>
+        Choose which dApp you`d like to play first
+      </div>
+      <Dock apps={props.apps} iconClicked={set_selection} />
 
       <div className={`container section topGap ${styles.linksContainer}`}>
         <div className={styles.dAppDockTitle}>
-          {selection !== -1 ? props.apps[selection].name : ''}
+          {selection !== -1 ? props.apps[selection].name : ""}
         </div>
-        <div >
-          {selection !== -1 ? props.apps[selection].text : ''}
-        </div>
+        <div>{selection !== -1 ? props.apps[selection].text : ""}</div>
         <div className={`bottomGap topGap ${styles.links}`}>
           {links?.map((link, index) => (
-              <>
-                <div className={`${styles.nodeDetails}`}>
-                  <div className={styles.nodeDetailsLine}><strong>Node {index}</strong></div>
-                  <div className={styles.nodeDetailsLine}>
-                    <div className={styles.nodeDetailsTitle}><strong>Url:</strong></div>
-                    <div className={styles.nodeDetailsData}><a href={link} target="_blank" rel="noreferrer" >{link}</a></div>
-                    <div className={styles.nodeDetailsCopy}><CopyButton copy={link}/></div>
-                  </div>
-                  <div className={styles.nodeDetailsLine}>
-                    <div className={styles.nodeDetailsTitle}><strong>Peer ID:</strong></div>
-                    <div className={styles.nodeDetailsData}>{props.peerIds?.length > 0 ? props.peerIds[index] : 'loading...' }</div>
-                    {props.peerIds[index] && <div className={styles.nodeDetailsCopy}><CopyButton copy={props.peerIds[index]}/></div>}
-                  </div>
-                  {
-                    selection !== -1 && props.apps[selection].name === 'hoprd-admin' &&
-                    <div className={styles.nodeDetailsLine}>
-                      <div className={styles.nodeDetailsTitle}><strong>API key:</strong></div>
-                      <div className={styles.nodeDetailsData}>{props.clusters[index].api_token}</div>
-                      <div className={styles.nodeDetailsCopy}><CopyButton copy={props.clusters[index].api_token}/></div>
-                    </div>
-                  }
+            <>
+              <div className={`${styles.nodeDetails}`}>
+                <div className={styles.nodeDetailsLine}>
+                  <strong>Node {index}</strong>
                 </div>
-              </>
+                <div className={styles.nodeDetailsLine}>
+                  <div className={styles.nodeDetailsTitle}>
+                    <strong>Url:</strong>
+                  </div>
+                  <div className={styles.nodeDetailsData}>
+                    <a href={link} target="_blank" rel="noreferrer">
+                      {link}
+                    </a>
+                  </div>
+                  <div className={styles.nodeDetailsCopy}>
+                    <CopyButton copy={link} />
+                  </div>
+                </div>
+                <div className={styles.nodeDetailsLine}>
+                  <div className={styles.nodeDetailsTitle}>
+                    <strong>Peer ID:</strong>
+                  </div>
+                  <div className={styles.nodeDetailsData}>
+                    {props.peerIds?.length > 0
+                      ? props.peerIds[index]
+                      : "loading..."}
+                  </div>
+                  {props.peerIds[index] && (
+                    <div className={styles.nodeDetailsCopy}>
+                      <CopyButton copy={props.peerIds[index]} />
+                    </div>
+                  )}
+                </div>
+                {selection !== -1 &&
+                  props.apps[selection].name === "hoprd-admin" && (
+                    <div className={styles.nodeDetailsLine}>
+                      <div className={styles.nodeDetailsTitle}>
+                        <strong>API key:</strong>
+                      </div>
+                      <div className={styles.nodeDetailsData}>
+                        {props.clusters[index].api_token}
+                      </div>
+                      <div className={styles.nodeDetailsCopy}>
+                        <CopyButton copy={props.clusters[index].api_token} />
+                      </div>
+                    </div>
+                  )}
+              </div>
+            </>
           ))}
         </div>
       </div>
